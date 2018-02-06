e23514ecc7c5f79aa2bf802d42aa922e277c5354
andresMP <marinpittet@gmail.com>
2018-02-06 17:48:05 -0300

    adicion form emergentes en empresas

--- script.js
+++ script.js
@@ -14,12 +14,40 @@ $( document ).ready(function() {
     }
   });
 
+   // Formulario reporte de empresas
+  $('#add-empresa').click(function() {
+    $('#add-empresaForm').fadeToggle();
+  })
+  $(document).mouseup(function (e) {
+    var container = $("#add-empresaForm");
+
+    if (container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.fadeOut();
+    }
+  });
+
   // Formulario verificacion de cheques
   $('#add-cheque').click(function() {
     $('#add-chequeForm').fadeToggle();
   })
   $(document).mouseup(function (e) {
     var container = $("#add-chequeForm");
+
+    if (container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.fadeOut();
+    }
+  });
+  
+  // Formulario verificacion de cheques empresas
+  $('#add-chequeEmpresa').click(function() {
+    $('#add-chequeEmpresaForm').fadeToggle();
+  })
+  $(document).mouseup(function (e) {
+    var container = $("#add-chequeEmpresaForm");
 
     if (container.is(e.target) // if the target of the click isn't the container...
         && container.has(e.target).length === 0) // ... nor a descendant of the container
@@ -34,6 +62,20 @@ $( document ).ready(function() {
   })
   $(document).mouseup(function (e) {
     var container = $("#add-comercialForm");
+
+    if (container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.fadeOut();
+    }
+  });
+
+  // Certificado de Antecedentes Comerciales empresas
+  $('#add-comercialEmpresa').click(function() {
+    $('#add-comercialEmpresaForm').fadeToggle();
+  })
+  $(document).mouseup(function (e) {
+    var container = $("#add-comercialEmpresaForm");
 
     if (container.is(e.target) // if the target of the click isn't the container...
         && container.has(e.target).length === 0) // ... nor a descendant of the container
